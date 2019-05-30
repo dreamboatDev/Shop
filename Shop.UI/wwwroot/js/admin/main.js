d5596e9c1d6fcce410be452f7fac440b5e6b172a
Alex <o.rozhmanov@gmail.com>
2019-05-30 13:41:55 +0300

    Add Vue.js

---
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -0,0 +1,22 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        price: 0,
+        showPrice: true
+    },
+    methods: {
+        tooglePrice: function () {
+            this.showPrice = !this.showPrice;
+        },
+        alert(x) {
+            alert(x);
+        }
+    },
+    computed: {
+        formatPrice: function () {
+            return "₴" + this.price;
+        }
+    }
+});
+
+
