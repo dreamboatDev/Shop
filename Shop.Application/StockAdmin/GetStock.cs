9664f30064d2a2cb1222dc4558ad4b2c6542b513
Alex <o.rozhmanov@gmail.com>
2019-06-03 17:40:40 +0300

    Stock (part 15)

---
+++ Shop.Application/StockAdmin/GetStock.cs
@@ -0,0 +1,40 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.StockAdmin
+{
+    public class GetStock
+    {
+        private ApplicationDbContext _context;
+
+        public GetStock(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<StockViewModel> Do(int productId)
+        {
+            var stock = _context.Stock
+                .Where(x => x.ProductId == productId)
+                .Select(x => new StockViewModel
+                {
+                    Id = x.Id,
+                    Description= x.Description,
+                    Quantity = x.Quantity,
+                })
+                .ToList();
+
+            return stock;
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
---
+++ Shop.Application/StockAdmin/UpdateStock.cs
@@ -0,0 +1,63 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.StockAdmin
+{
+    class UpdateStock
+    {
+        private ApplicationDbContext _context;
+
+        public UpdateStock(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response> Do(Request request)
+        {
+            var stocks = new List<Stock>();
+
+            foreach (var stock in request.Stock)
+            {
+                stocks.Add(new Stock
+                {
+
+                    Id = stock.Id,
+                    Description = stock.Description,
+                    Quantity = stock.Quantity,
+                    ProductId = stock.ProductId
+                });
+            }
+
+            _context.Stock.UpdateRange(stocks);
+            await _context.SaveChangesAsync();
+
+            return new Response
+            {
+                Stock = request.Stock
+            };
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public int ProductId { get; set; }
+        }
+
+        public class Request
+        {
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+
+        public class Response
+        {
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+    }
+}
